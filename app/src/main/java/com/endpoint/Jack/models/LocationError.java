a4a9e19484ed2b911835e3a47a0d4414e3f68505
End Point <ahmedmohamed23345@gmail.com>
2020-03-15 14:51:53 +0200

    ch

---
+++ app/src/main/java/com/endpoint/Jack/models/LocationError.java
@@ -0,0 +1,14 @@
+package com.endpoint.Jack.models;
+
+public class LocationError {
+
+    private int status;
+
+    public LocationError(int status) {
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
