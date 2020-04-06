6a3302c22a70dcd59fa2f20859dc40545afe7f40
End Point <ahmedmohamed23345@gmail.com>
2020-04-06 13:50:17 +0200

    ch

--- app/src/main/java/com/endpoint/Jack/activities_fragments/activity_fragment_category/CategoryActivity.java
+++ app/src/main/java/com/endpoint/Jack/activities_fragments/activity_fragment_category/CategoryActivity.java
@@ -1,20 +1,29 @@
 package com.endpoint.Jack.activities_fragments.activity_fragment_category;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.PorterDuff;
 import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -42,10 +51,13 @@ import com.endpoint.Jack.preferences.Preferences;
 import com.endpoint.Jack.remote.Api;
 import com.endpoint.Jack.share.Common;
 import com.endpoint.Jack.tags.Tags;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.iarcuschin.simpleratingbar.SimpleRatingBar;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Locale;
@@ -63,6 +75,7 @@ public class CategoryActivity extends AppCompatActivity {
     private String lang;
     private TextView tv_name, tv_content, tv_rate, tv_addess, tv_address, tv_time, tv_status;
     private CircleImageView imageView;
+    private ImageView image_details;
     private SimpleRatingBar simpleRatingBar;
     private ConstraintLayout cons_add_coupon;
     private ViewPager pager;
@@ -84,6 +97,12 @@ public class CategoryActivity extends AppCompatActivity {
 private Preferences preference;
 private UserModel userModel;
     private String order_details;
+    private FloatingActionButton fab;
+    private final int IMG1=3,IMG2=4;
+    private Uri uri=null;
+    private final String read_permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private final String write_permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    private final String camera_permission = Manifest.permission.CAMERA;
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -127,6 +146,7 @@ private UserModel userModel;
         ll_change = findViewById(R.id.ll_change);
         ll_choose_delivery_time=findViewById(R.id.ll_time);
         tv_addess = findViewById(R.id.tv_address);
+        fab = findViewById(R.id.fab);
         tv_address = findViewById(R.id.tv_address1);
         edt_order_details = findViewById(R.id.edt_order_details);
         arrow1 = findViewById(R.id.arrow1);
@@ -136,6 +156,7 @@ private UserModel userModel;
         progressBar = findViewById(R.id.progBarSlider);
         btnOrderNow = findViewById(R.id.btnOrderNow);
         cons_add_coupon = findViewById(R.id.cons_add_coupon);
+        image_details = findViewById(R.id.image_details);
         cons_add_coupon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -152,7 +173,13 @@ private UserModel userModel;
                 }
             }
         });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                CreateImageAlertDialog();
+            }
+        });
         progressBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
         if (lang.equals("en")) {
             arrow1.setRotation(180.0f);
@@ -467,6 +494,81 @@ private UserModel userModel;
         else if(requestCode==2){
             userModel=preference.getUserData(this);
         }
+        if (requestCode == IMG1 && resultCode == Activity.RESULT_OK && data!=null)
+        {
+            uri = data.getData();
+            File file = new File(Common.getImagePath(this,uri));
+            try {
+                Picasso.with(this).load(file).into(image_details);
+
+            }catch (Exception e)
+            {
+                Picasso.with(this).load(uri).into(image_details);
+            }
+            image_details.setVisibility(View.VISIBLE);
+
+        }else if (requestCode == IMG2 && resultCode == Activity.RESULT_OK && data!=null)
+        {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            image_details.setImageBitmap(bitmap);
+            uri = getUriFromBitmap(bitmap);
+            image_details.setVisibility(View.VISIBLE);
+
+        }
+
+    }
+
+    private Uri getUriFromBitmap(Bitmap bitmap)
+    {
+        String path = "";
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream);
+            path = MediaStore.Images.Media.insertImage(this.getContentResolver(),bitmap,"title",null);
+            return Uri.parse(path);
+
+        }catch (SecurityException e)
+        {
+            Toast.makeText(this,getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+
+        }catch (Exception e)
+        {
+            Toast.makeText(this,getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+
+        }
+        return null;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == IMG1)
+        {
+            if (grantResults.length>0)
+            {
+                if (grantResults[0]== PackageManager.PERMISSION_GRANTED)
+                {
+                    select_photo(1);
+                }else
+                {
+                    Toast.makeText(this,getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }else if (requestCode == IMG2)
+        {
+            if (grantResults.length>0)
+            {
+                if (grantResults[0]==PackageManager.PERMISSION_GRANTED&&grantResults[1]==PackageManager.PERMISSION_GRANTED)
+                {
+                    select_photo(2);
+
+                }else
+                {
+                    Toast.makeText(this,getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
 
     }
     public  void CreateAlertDialog(String order_id)
@@ -512,6 +614,120 @@ finish();
         dialog.setView(view);
         dialog.show();
     }
+    private void CreateImageAlertDialog()
+    {
 
+        final androidx.appcompat.app.AlertDialog dialog = new androidx.appcompat.app.AlertDialog.Builder(this)
+                .setCancelable(true)
+                .create();
+
+
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_select_image,null);
+        Button btn_camera = view.findViewById(R.id.btn_camera);
+        Button btn_gallery = view.findViewById(R.id.btn_gallery);
+        Button btn_cancel = view.findViewById(R.id.btn_cancel);
+
+
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Check_CameraPermission();
+
+            }
+        });
+
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Check_ReadPermission();
+
+
+
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.getWindow().getAttributes().windowAnimations= R.style.dialog_congratulation_animation;
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setView(view);
+        dialog.show();
+    }
+    private void Check_ReadPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this,read_permission)!= PackageManager.PERMISSION_GRANTED )
+        {
+            ActivityCompat.requestPermissions(this,new String[]{read_permission},IMG1);
+        }else
+        {
+            select_photo(1);
+
+        }
+
+    }
+
+
+    private void Check_CameraPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this,camera_permission)!= PackageManager.PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(this,write_permission)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{camera_permission,write_permission},IMG2);
+        }else
+        {
+            select_photo(2);
+
+        }
+
+    }
+    private void select_photo(int type)
+    {
+        Intent  intent = new Intent();
+
+        if (type == 1)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            {
+                intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+            }else
+            {
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+            }
+
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setType("image/*");
+            startActivityForResult(intent,IMG1);
+
+        }else if (type ==2)
+        {
+            try {
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,IMG2);
+            }catch (SecurityException e)
+            {
+                Toast.makeText(this,R.string.perm_image_denied, Toast.LENGTH_SHORT).show();
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this,R.string.perm_image_denied, Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+        }
+
+
+
+    }
 
 }
